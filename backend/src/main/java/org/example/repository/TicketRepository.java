7fd12f47b6982d8f57c71fead82129392ea4f8cc
Navsika <ermolaevauliana05@yandex.ru>
2026-05-17 01:05:08 +0800

    feat: remove method for capacity

--- backend/src/main/java/org/example/repository/TicketRepository.java
+++ backend/src/main/java/org/example/repository/TicketRepository.java
@@ -2,22 +2,14 @@ package org.example.repository;
 
 import org.example.entity.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface TicketRepository extends JpaRepository<Ticket, Integer> {
     List<Ticket> findByFlightId(Integer flightId);
     long countByFlightId(Integer flightId);
-    @Query("SELECT m.passengerCapacity FROM Flight f " +
-            "JOIN Aircraft a ON f.aircraftId = a.aircraftId " +
-            "JOIN AircraftsModel m ON a.modelId = m.modelId " +
-            "WHERE f.flightId = :flightId")
-    Optional<Integer> findAircraftCapacityByFlightId(@Param("flightId") Integer flightId);
 
     boolean existsByPassengerIdAndFlightId(Integer passengerId, Integer flightId);
 
