7246e0c5a94725d5b0d0eee78b5e5eccc83801e3
Navsika <ermolaevauliana05@yandex.ru>
2026-05-22 03:09:25 +0800

    fix: fix logic?

--- backend/src/main/java/org/example/repository/FlightRepository.java
+++ backend/src/main/java/org/example/repository/FlightRepository.java
@@ -38,13 +38,13 @@ public interface FlightRepository extends JpaRepository<Flight, Integer>, JpaSpe
             "JOIN Aircraft a ON f.aircraftId = a.aircraftId " +
             "JOIN AircraftModel m ON a.modelId = m.modelId " +
             "WHERE f.flightId = :flightId")
-    Optional<Integer> findAircraftCapacityByFlightId(@Param("flightId") Integer flightId);
+    Integer  findAircraftCapacityByFlightId(@Param("flightId") Integer flightId);
 
     @Query("SELECT m.cargoCapacity FROM Flight f " +
             "JOIN Aircraft a ON f.aircraftId = a.aircraftId " +
             "JOIN AircraftModel m ON a.modelId = m.modelId " +
             "WHERE f.flightId = :flightId")
-    Optional<Integer> findAircraftCargoCapacityByFlightId(@Param("flightId") Integer flightId);
+    Integer findAircraftCargoCapacityByFlightId(@Param("flightId") Integer flightId);
 
     boolean existsByScheduleIdAndScheduledDeparture(Integer scheduleId, OffsetDateTime scheduledDeparture);
 
